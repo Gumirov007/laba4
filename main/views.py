bb6f0b03c7f6115f136d9adef596d1326fdd0654
Gumirov007 <105052415+Gumirov007@users.noreply.github.com>
2024-10-09 09:49:39 +0300

    Add files via upload

---
+++ main/views.py
@@ -0,0 +1,18 @@
+import requests
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+
+def home_page(request: HttpRequest) -> HttpResponse:
+    url = "https://dog.ceo/api/breeds/image/random/10"
+    response = requests.get(url)
+    data = response.json()
+    images = data['message']
+    return render(request, 'main/index.html', {'images': images})
+
+def about(request: HttpRequest) -> HttpResponse:
+    return render(request, 'main/about.html')
+
+
+def contact(request: HttpRequest) -> HttpResponse:
+    return render(request, 'main/contact.html')
